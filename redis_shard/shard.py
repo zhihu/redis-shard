1a8c7b9b361a3b9caab0239baabdb3a94ce3eedd
SuNan <su@zhihu.com>
2011-08-10 16:07:57 +0800

    add hset_in, hget_in, lpop_in and rpush for newsearch

--- redis_shard/shard.py
+++ redis_shard/shard.py
@@ -58,6 +58,40 @@ class RedisShardAPI(object):
         f = getattr(server, method)
         return f(*args, **kwargs)
 
+    def __hop_in(self, method, *args, **kwargs):
+        '''
+        使用field作为查询hashring的key
+        '''
+        try:
+            key = args[1]
+            assert isinstance(key, basestring)
+        except:
+            raise ValueError("method '%s' requires a key param as the second argument" % method)
+        server = self.get_server(key)
+        if method == "hget_in":
+            method = "hget"
+        elif method == "hset_in":
+            method = "hset"
+        else:
+            print "you can't be here"
+        f = getattr(server, method)
+        return f(*args, **kwargs)
+        
+    def __qop_in(self, method, *args, **kwargs):
+        '''
+        指定key值所对应的hashring上的一个节点作为队列服务器
+        '''    
+        key = "queue"
+        server = self.get_server(key)
+        if method == "rpush_in":
+            method = "rpush"
+        elif method == "lpop_in":
+            method = "lpop"
+        else:
+            print "you can't be here"
+        f = getattr(server, method)
+        return f(*args, **kwargs)
+        
     def __getattr__(self, method):
         if method in [
             "get", "set", "getset",
@@ -77,6 +111,10 @@ class RedisShardAPI(object):
             "publish","brpop","blpop","lpush","rpush",
             ]:
             return functools.partial(self.__wrap, method)
+        elif method in ["hget_in", "hset_in"]:
+            return functools.partial(self.__hop_in, method)
+        elif method in ["lpop_in", "rpush_in"]:
+            return functools.partial(self.__qop_in, method)
         else:
             raise NotImplementedError("method '%s' cannot be sharded" % method)
 
