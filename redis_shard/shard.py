6c8af98428b5fef17e5cba9a9599bd7aa95d6fc5
Young King <yanckin@gmail.com>
2012-08-22 16:06:03 +0800

    add lock method

--- redis_shard/shard.py
+++ redis_shard/shard.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import re
 import redis
+from redis.client import Lock
 from hashring import HashRing
 import functools
 
@@ -189,3 +190,17 @@ class RedisShardAPI(object):
             for i in range(len(field_list)):
                 result[field_list[i]]=value[i]
         return result
+
+    def lock(self, name, timeout=None, sleep=0.1):
+        """
+        Return a new Lock object using key ``name`` that mimics
+        the behavior of threading.Lock.
+
+        If specified, ``timeout`` indicates a maximum life for the lock.
+        By default, it will remain locked until release() is called.
+
+        ``sleep`` indicates the amount of time to sleep per loop iteration
+        when the lock is in blocking mode and another client is currently
+        holding the lock.
+        """
+        return Lock(self, name, timeout=timeout, sleep=sleep)
