b017248ff15b3aaf89e7627d74edfee0ecc3c88a
Rylan Hazelton <rylan@badger.com>
2012-08-27 17:50:29 -0700

    change to most recent hash_ring and change to use sha1 for better distro

--- redis_shard/hashring.py
+++ redis_shard/hashring.py
@@ -1,80 +1,159 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 """
- consistent hashing for nosql client
- based on ezmobius client (redis-rb)
- and see this article http://amix.dk/blog/viewEntry/19367
+    hash_ring
+    ~~~~~~~~~~~~~~
+    Implements consistent hashing that can be used when
+    the number of server nodes can increase or decrease (like in memcached).
+
+    Consistent hashing is a scheme that provides a hash table functionality
+    in a way that the adding or removing of one slot
+    does not significantly change the mapping of keys to slots.
+
+    More information about consistent hashing can be read in these articles:
+
+        "Web Caching with Consistent Hashing":
+            http://www8.org/w8-papers/2a-webserver/caching/paper2.html
+
+        "Consistent hashing and random trees:
+        Distributed caching protocols for relieving hot spots on the World Wide Web (1997)":
+            http://citeseerx.ist.psu.edu/legacymapper?did=38148
+
+
+    Example of usage::
+
+        memcache_servers = ['192.168.0.246:11212',
+                            '192.168.0.247:11212',
+                            '192.168.0.249:11212']
+
+        ring = HashRing(memcache_servers)
+        server = ring.get_node('my_key')
+
+    :copyright: 2008 by Amir Salihefendic.
+    :license: BSD
 """
 
-import zlib
-import bisect
+import hashlib
+import math
+from bisect import bisect
 
 class HashRing(object):
-    """Consistent hash for nosql API"""
-    def __init__(self, nodes=[], replicas=128):
-        """Manages a hash ring.
 
-        `nodes` is a list of objects that have a proper __str__ representation.
-        `replicas` indicates how many virtual points should be used pr. node,
-        replicas are required to improve the distribution.
+    def __init__(self, nodes=None, weights=None):
+        """`nodes` is a list of objects that have a proper __str__ representation.
+        `weights` is dictionary that sets weights to the nodes.  The default
+        weight is that all nodes are equal.
         """
-        self.nodes = []
-        self.replicas=replicas
-        self.ring={}
-        self.sorted_keys=[]
+        self.ring = dict()
+        self._sorted_keys = []
 
-        for n in nodes:
-            self.add_node(n)
+        self.nodes = nodes
 
-    def add_node(self, node):
-        """Adds a `node` to the hash ring (including a number of replicas).
+        if not weights:
+            weights = {}
+        self.weights = weights
+
+        self._generate_circle()
+
+    def _generate_circle(self):
+        """Generates the circle.
         """
-        self.nodes.append(node)
-        for x in xrange(self.replicas):
-            crckey = zlib.crc32("%s:%d" % (node, x))
-            self.ring[crckey] = node
-            self.sorted_keys.append(crckey)
-        
-        self.sorted_keys.sort() 
+        total_weight = 0
+        for node in self.nodes:
+            total_weight += self.weights.get(node, 1)
 
-    def remove_node(self, node):
-        """Removes `node` from the hash ring and its replicas.
-        """
-        self.nodes.remove(node)
-        for x in xrange(self.replicas):
-            crckey = zlib.crc32("%s:%d" % (node, x))
-            self.ring.remove(crckey)
-            self.sorted_keys.remove(crckey)
-        
-    def get_node(self, key):
+        for node in self.nodes:
+            weight = 1
+
+            if node in self.weights:
+                weight = self.weights.get(node)
+
+            factor = math.floor((40*len(self.nodes)*weight) / total_weight);
+
+            for j in xrange(0, int(factor)):
+                b_key = self._hash_digest( '%s-%s' % (node, j) )
+
+                for i in xrange(0, 3):
+                    key = self._hash_val(b_key, lambda x: x+i*4)
+                    self.ring[key] = node
+                    self._sorted_keys.append(key)
+
+        self._sorted_keys.sort()
+
+    def get_node(self, string_key):
         """Given a string key a corresponding node in the hash ring is returned.
 
         If the hash ring is empty, `None` is returned.
         """
-        n, i = self.get_node_pos(key)
-        return n
+        pos = self.get_node_pos(string_key)
+        if pos is None:
+            return None
+        return self.ring[ self._sorted_keys[pos] ]
 
-    def get_node_pos(self, key):
+    def get_node_pos(self, string_key):
         """Given a string key a corresponding node in the hash ring is returned
         along with it's position in the ring.
 
         If the hash ring is empty, (`None`, `None`) is returned.
         """
-        if len(self.ring) == 0:
-            return [None, None]
-        crc = zlib.crc32(key)
-        idx = bisect.bisect(self.sorted_keys, crc)
-        idx = min(idx, (self.replicas * len(self.nodes))-1) # prevents out of range index
-        return [self.ring[self.sorted_keys[idx]], idx]
+        if not self.ring:
+            return None
 
-    def iter_nodes(self, key):
+        key = self.gen_key(string_key)
+
+        nodes = self._sorted_keys
+        pos = bisect(nodes, key)
+
+        if pos == len(nodes):
+            return 0
+        else:
+            return pos
+
+    def iterate_nodes(self, string_key, distinct=True):
         """Given a string key it returns the nodes as a generator that can hold the key.
+
+        The generator iterates one time through the ring
+        starting at the correct position.
+
+        if `distinct` is set, then the nodes returned will be unique,
+        i.e. no virtual copies will be returned.
         """
-        if len(self.ring) == 0: yield None, None
-        node, pos = self.get_node_pos(key)
-        for k in self.sorted_keys[pos:]:
-            yield k, self.ring[k]
-    
-    def __call__(self, key):
-        return self.get_node(key)
+        if not self.ring:
+            yield None, None
+
+        returned_values = set()
+        def distinct_filter(value):
+            if str(value) not in returned_values:
+                returned_values.add(str(value))
+                return value
+
+        pos = self.get_node_pos(string_key)
+        for key in self._sorted_keys[pos:]:
+            val = distinct_filter(self.ring[key])
+            if val:
+                yield val
+
+        for i, key in enumerate(self._sorted_keys):
+            if i < pos:
+                val = distinct_filter(self.ring[key])
+                if val:
+                    yield val
+
+    def gen_key(self, key):
+        """Given a string key it returns a long value,
+        this long value represents a place on the hash ring.
+
+        sha1 is currently used because it mixes well.
+        """
+        b_key = self._hash_digest(key)
+        return self._hash_val(b_key, lambda x: x)
+
+    def _hash_val(self, b_key, entry_fn):
+        return (( b_key[entry_fn(3)] << 24)
+                |(b_key[entry_fn(2)] << 16)
+                |(b_key[entry_fn(1)] << 8)
+                | b_key[entry_fn(0)] )
+
+    def _hash_digest(self, key):
+        m = hashlib.sha1()
+        m.update(key)
+        return map(ord, m.digest())
