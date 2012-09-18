a2ff6194227d82963c12eac4562387a9dfc7ca57
YoungKing <yanckin@gmail.com>
2012-09-18 12:56:27 +0800

    update config for travis-ci tests
    the travis-ci now only support single redis instance

--- tests/config.py
+++ tests/config.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# you should use multiple instance of redis, here just for travis-ci tests
 servers = [
     {'name':'node1', 'host':'127.0.0.1', 'port':6379, 'db':0},
-    {'name':'node2', 'host':'127.0.0.1', 'port':6380, 'db':0},
-    {'name':'node3', 'host':'127.0.0.1', 'port':6381, 'db':0},
+    {'name':'node2', 'host':'127.0.0.1', 'port':6379, 'db':1},
+    {'name':'node3', 'host':'127.0.0.1', 'port':6379, 'db':2},
 ]
 
 try:
