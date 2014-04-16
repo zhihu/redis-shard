56844be73f49a953ebbc4fd6f22b1e77216177e4
PSJay <psjay.peng@gmail.com>
2014-04-16 15:43:02 +0800

    Keeping pipeline results order.

--- redis_shard/__init__.py
+++ redis_shard/__init__.py
@@ -2,4 +2,4 @@
 Based on this article: http://antirez.com/post/redis-presharding.html
 """
 
-__version__ = '0.2.1'
+__version__ = '0.2.2a1'
--- redis_shard/pipeline.py
+++ redis_shard/pipeline.py
@@ -8,12 +8,21 @@ class Pipeline(object):
     def __init__(self, shard_api):
         self.shard_api = shard_api
         self.pipelines = {}
+        self.__counter = 0
+        self.__indexes = {}
 
     def get_pipeline(self, key):
         name = self.shard_api.get_server_name(key)
         if name not in self.pipelines:
             self.pipelines[name] = self.shard_api.connections[name].pipeline()
         return self.pipelines[name]
+
+    def __record_index(self, pipeline):
+        if pipeline not in self.__indexes:
+            self.__indexes[pipeline] = [self.__counter]
+        else:
+            self.__indexes[pipeline].append(self.__counter)
+        self.__counter += 1
 
     def __wrap(self, method, *args, **kwargs):
         try:
@@ -23,7 +32,9 @@ class Pipeline(object):
             raise ValueError("method '%s' requires a key param as the first argument" % method)
         pipeline = self.get_pipeline(key)
         f = getattr(pipeline, method)
-        return f(*args, **kwargs)
+        r = f(*args, **kwargs)
+        self.__record_index(pipeline)
+        return r
 
     def __wrap_eval(self, method, script_or_sha, numkeys, *keys_and_args):
         if numkeys != 1:
@@ -31,7 +42,9 @@ class Pipeline(object):
         key = keys_and_args[0]
         pipeline = self.get_pipeline(key)
         f = getattr(pipeline, method)
-        return f(script_or_sha, numkeys, *keys_and_args)
+        r = f(script_or_sha, numkeys, *keys_and_args)
+        self.__record_index(pipeline)
+        return r
 
     def __wrap_tag(self, method, *args, **kwargs):
         key = args[0]
@@ -43,13 +56,22 @@ class Pipeline(object):
             raise ValueError("method '%s' requires tag key params as its arguments" % method)
         method = method.lstrip("tag_")
         f = getattr(pipeline, method)
-        return f(*args, **kwargs)
+        r = f(*args, **kwargs)
+        self.__record_index(pipeline)
+        return r
 
     def execute(self):
         results = []
         for name, pipeline in iteritems(self.pipelines):
             result = pipeline.execute()
-            results.extend(list(result))
+            result = zip(self.__indexes[pipeline], result)
+            results.extend(result)
+
+        self.__counter = 0
+        self.__indexes = {}
+
+        results.sort(key=lambda x: x[0])
+        results = [r[1] for r in results]
         return results
 
     def __getattr__(self, method):
