743eff7860a840e6b611b81635c3e8e5f90d2041
YoungKing <yanckin@gmail.com>
2014-08-05 12:15:00 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     tests_require=['Nose'],
     classifiers=[
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2"
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.3",
