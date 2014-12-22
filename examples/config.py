451b086d5d78a77318146a6aa00c766b4b15a7d3
acmerfight <acmerfight@gmail.com>
2014-12-22 18:16:15 +0800

    make example more distinct

--- examples/config.py
+++ examples/config.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 servers = [
-    {'name': 'server1', 'host': '127.0.0.1', 'port': 6379, 'db': 0},
-    {'name': 'server2', 'host': '127.0.0.1', 'port': 6379, 'db': 0},
-    {'name': 'server3', 'host': '127.0.0.1', 'port': 6379, 'db': 0},
+    {'name': 'server1', 'host': '127.0.0.1', 'port': 6376, 'db': 0},
+    {'name': 'server2', 'host': '127.0.0.1', 'port': 6377, 'db': 0},
+    {'name': 'server3', 'host': '127.0.0.1', 'port': 6378, 'db': 0},
     {'name': 'server4', 'host': '127.0.0.1', 'port': 6379, 'db': 0},
 ]
