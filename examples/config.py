3662857e643b39b66dce19ff0f5a8197f9cec6c0
acmerfight <acmerfight@gmail.com>
2014-12-22 18:06:03 +0800

    fix code format

--- examples/config.py
+++ examples/config.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 servers = [
-        {'name':'server1','host':'127.0.0.1','port':6379,'db':0},
-        {'name':'server2','host':'127.0.0.1','port':6379,'db':0},
-        {'name':'server3','host':'127.0.0.1','port':6379,'db':0},
-        {'name':'server4','host':'127.0.0.1','port':6379,'db':0},
-        #{'name':'server5','host':'127.0.0.1','port':14000,'db':0},
-        #{'name':'server6','host':'127.0.0.1','port':15000,'db':0},
-        ]
+    {'name': 'server1', 'host': '127.0.0.1', 'port': 6379, 'db': 0},
+    {'name': 'server2', 'host': '127.0.0.1', 'port': 6379, 'db': 0},
+    {'name': 'server3', 'host': '127.0.0.1', 'port': 6379, 'db': 0},
+    {'name': 'server4', 'host': '127.0.0.1', 'port': 6379, 'db': 0},
+]
